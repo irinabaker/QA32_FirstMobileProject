c265f824aa9b282cfcfd411125b5c0655b6bd669
Ira Baker <49585685+irinabaker@users.noreply.github.com>
2022-10-05 12:02:23 +0200

    Added TestBase, LaunchAppTests

---
+++ src/test/java/com/telran/remindly/tests/MainPageTests.java
@@ -0,0 +1,11 @@
+package com.telran.remindly.tests;
+
+import org.testng.annotations.Test;
+
+public class MainPageTests extends TestBase{
+
+    @Test
+    public void launchAppTest() {
+        System.out.println("App launch!");
+    }
+}
